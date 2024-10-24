17985fe0115cb0e212763d5bcf3685737d848c80
Chandrika Sista <cguntur@gmail.com>
2024-10-23 17:23:45 -0700

    Added a responsive nav menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,13 @@
 //import { Link, useLocation } from 'react-router-dom';
 
-//import Nav from './NavTabs';
-//import NavList from './NavList';
+import NavList from './NavList';
 
 function Header() {
     //const currentPage = useLocation().pathname;
     return (
         <div className="header">
             <h1 className="title"><a href="/">My Recipes</a></h1>
-            
+            <NavList />
         </div>
     )
 }  
